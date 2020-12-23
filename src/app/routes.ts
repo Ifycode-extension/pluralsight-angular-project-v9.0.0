c4f3cccb676b98f33d49020de3d232b5a9b6b6ae
ifycode <marychord@gmail.com>
2020-12-23 02:09:47 -0800

    Add routerLink

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 
 export const appRoutes: Routes = [
-  { path: 'product', component: ProductListComponent },
-  { path: 'product/description', component: ProductPageComponent },
-  { path: '', redirectTo: '/product', pathMatch: 'full' }
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' }
 ]
